30a12aa7017eb1d42fb9e951a5f786237f981be4
gilliam.zhao <gilliam.zhao@dianping.com>
2020-09-02 08:55:47 +0800

    symbol ['render to dom']

--- toy-react.js
+++ toy-react.js
@@ -1,3 +1,5 @@
+const RENDER_TO_DOM = Symbol('render to dom');
+
 class ElementWrapper {
     constructor(type) {
         this.root = document.createElement(type);
@@ -8,13 +10,26 @@ class ElementWrapper {
     }
 
     appendChild(component) {
-        this.root.appendChild(component.root);
+        let range = document.createRange();
+        range.setStart(this.root, this.root.childNodes.length);
+        range.setEnd(this.root, this.root.childNodes.length);
+        component[RENDER_TO_DOM](range);
+    }
+
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 }
 
 class TextWrapper {
     constructor(content) {
         this.root = document.createTextNode(content);
+    }
+
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 }
 
@@ -33,11 +48,8 @@ export class Component {
         this.children.push(component);
     }
 
-    get root() {
-        if (!this._root) {
-            this._root = this.render().root;
-        }
-        return this._root;
+    [RENDER_TO_DOM](range) {
+        this.render()[RENDER_TO_DOM](range);
     }
 }
 
@@ -73,6 +85,10 @@ export function createElement(type, attributes, ...children) {
     return el;
 }
 
-export function render(component, container) {
-    container.appendChild(component.root);
+export function render(component, parentElement) {
+    let range = document.createRange();
+    range.setStart(parentElement, 0);
+    range.setEnd(parentElement, parentElement.childNodes.length);
+    range.deleteContents();
+    component[RENDER_TO_DOM](range);
 }
